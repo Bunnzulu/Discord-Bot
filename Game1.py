f755c42fb0e466d4ada08be8bd35cad7029b49c5
Nobody <bunnza2020@gmail.com>
2024-07-20 03:48:32 -0500

    Game 1

---
+++ Game1.py
@@ -0,0 +1,22 @@
+import random
+
+class NumberGuesserGame():
+    def __init__(self):
+        self.HighNumber = 0
+        self.LowNumber = 0
+        self.Number = 0
+        self.AIHighNumber = 0
+        self.AILowNumber = 0
+        self.Turn = ""
+        self.NumberGuessed = False
+        self.Active = False
+        self.UserGuesses = 0
+    
+    def Pick_random_Number(self):
+        self.Number = random.randint(self.LowNumber,self.HighNumber)
+    
+    def UserGuess(self,Guess:int) -> str:
+        self.UserGuesses += 1
+        if Guess > self.Number:return "Too High"
+        if Guess == self.Number:return "That's it"
+        else:return "Too Low"
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import discord 
 from dotenv import load_dotenv
 import os
-import random
 import re
+from Game1 import NumberGuesserGame
 load_dotenv(".env")
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+Game1 = NumberGuesserGame()
 
 @client.event
 async def on_ready():
@@ -23,6 +25,10 @@ async def on_message(message):
         await message.channel.send('Hello!')
     
     if message.content.startswith('$Game1'):
+        Game1.Turn = "User"
+        Game1.Active = True
         await message.channel.send('Pick how high the number can go')
+    
+    
 
 client.run(os.getenv("TOKEN"))
