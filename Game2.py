593726722030d632e585a1425278a2d7829c76f6
Nobody <bunnza2020@gmail.com>
2024-07-25 02:41:27 -0500

    Update Game2.py

--- Game2.py
+++ Game2.py
@@ -15,9 +15,18 @@ class Wordle:
         self.Guesses += 1
         if guess.upper() == self.Word:return f"Correct!. The word was {self.Word}"
         else:
-            pass
+            for index,i in enumerate(self.Word): self.Check_Letter(i,index)
+            return "Incorrect"
     
-    def Check_Letter(self,letter,pos):
-        if letter == self.Word[pos]: self.WrongMessage["Green"] = [self.WrongMessage["Green"][0] + 1,self.WrongMessage["Green"][1].append(pos),self.WrongMessage["Green"][2] + self.Word[pos]]
-        elif letter in self.Word and self.WrongMessage["Yellow"][3]: 
-            self.WrongMessage["Yellow"] = [self.WrongMessage["Green"][0] + 1,pos]
+    def Check_Letter(self,letter:str,pos:int):
+        if letter == self.Word[pos]: 
+            self.WrongMessage["Green"] = [self.WrongMessage["Green"][0] + 1,self.WrongMessage["Green"][1].append(pos),self.WrongMessage["Green"][2] + letter]
+        elif letter in self.Word and ((letter not in self.WrongMessage["Yellow"][2] and letter not in self.WrongMessage["Green"][2]) or self.Word.count(letter) > 2): 
+            self.WrongMessage["Yellow"] = [self.WrongMessage["Yellow"][0] + 1,self.WrongMessage["Yellow"][1].append(pos),self.WrongMessage["Yellow"][2] + letter]
+        else:
+            self.WrongMessage["Red"] = [self.WrongMessage["Red"][0] + 1,self.WrongMessage["Red"][1].append(pos),self.WrongMessage["Red"][2] + letter]
+    
+    def Display_Green(self):
+        if self.WrongMessage["Green"][0] > 0:
+            pass
+
